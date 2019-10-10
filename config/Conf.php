352244b97f34f022bdd111bb3a9181aab7e3b6f1
HouillonAnd <43677104+HouillonAnd@users.noreply.github.com>
2019-10-10 20:18:28 +0200

    Conf, File, Model init

---
+++ config/Conf.php
@@ -0,0 +1,46 @@
+<?php
+class Conf {
+   
+  static private $databases = array(
+    // Le nom d'hote est webinfo a l'IUT
+    // ou localhost sur votre machine
+    'hostname' => 'webinfo',
+    // A l'IUT, vous avez une BDD nommee comme votre login
+    // Sur votre machine, vous devrez creer une BDD
+    'database' => 'agussolg',
+    // A l'IUT, c'est votre login
+    // Sur votre machine, vous avez surement un compte 'root'
+    'login' => 'agussolg',
+    // A l'IUT, c'est votre mdp (INE par defaut)
+    // Sur votre machine personelle, vous avez creez ce mdp a l'installation
+    'password' => 'AANLLPzq@'
+  );
+  // la variable debug est un boolean
+    static private $debug = True; 
+    
+    static public function getDebug() {
+      return self::$debug;
+    }
+
+    static public function getHostname() {
+    //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+    return self::$databases['hostname'];
+  }
+
+    static public function getDatabase() {
+    //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+    return self::$databases['database'];
+  }
+   
+  static public function getLogin() {
+    //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+    return self::$databases['login'];
+  }
+
+    static public function getPassword() {
+    //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+    return self::$databases['password'];
+  }
+   
+}
+?>
---
+++ lib/File.php
@@ -0,0 +1,15 @@
+<?php
+class File {
+	public static function build_path($path_array) {
+	    // $ROOT_FOLDER (sans slash à la fin) vaut
+	    // "/home/ann2/votre_login/public_html/TD5" à l'IUT 
+	    //$ROOT_FOLDER = "/home/ann2/houillona/public_html/PHP/TD5";
+
+		$DS = DIRECTORY_SEPARATOR;
+		$ROOT_FOLDER = __DIR__ . $DS . "..";
+
+
+	    return $ROOT_FOLDER. $DS . join($DS, $path_array);
+	}
+}
+?>
