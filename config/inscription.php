ec40cec7d1817ff596ac94d85b10ac058b5640ff
Leonard-Rivals <47136441+Leonard-Rivals@users.noreply.github.com>
2019-10-29 10:25:16 +0100

    Add files via upload
    Upload du fichier php inscription ( pas encore fini )

---
+++ config/inscription.php
@@ -0,0 +1,76 @@
+<?php
+require_once File::build_path(array('config', 'Conf.php'));
+
+?>
+
+
+<html>
+		<head>
+			<title>
+				
+			</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<div align="center">
+				<h2>Inscription</h2>
+				<br>
+				<br>
+				<br>
+				<form method="POST" action=""> 
+					<table>
+						<tr>
+							<td align="right">
+								<label for="login">Login :  </label>
+							</td>
+							<td align="right">
+								<input type="text" placeholder="Votre login" id="login" name="login"/>
+							</td>
+						</tr>
+						<tr>
+							<td align="right">
+								<label for="email">Email :  </label>
+							</td>
+							<td align="right">
+								<input type="email" placeholder="Votre email" id="email" name="email"/>
+							</td>
+						</tr>
+						<tr>
+							<td align="right">
+								<label for="email2">Confirmation du Email :  </label>
+							</td>
+							<td align="right">
+								<input type="email" placeholder="Confirmez votre email" id="email2" name="email2"/>
+							</td>
+						</tr>
+						<tr>
+							<td align="right">
+								<label for="password">Mot de Passe :  </label>
+							</td>
+							<td align="right">
+								<input type="password" placeholder="Votre Mot de Passe" id="password" name="password"/>
+							</td>
+						</tr>
+						<tr>
+		                  	<td align="right">
+		                    	<label for="password2">Confirmation du mot de passe :</label>
+			                </td>
+			                <td>
+		                     <input type="password" placeholder="Confirmez votre Mot de Passe" id="password2" name="password2" />
+		                    </td>
+		                </tr>
+		                <tr>
+                  			<td></td>
+                  			<td align="center">
+                     		<br />
+                     		<input type="submit" name="forminscription" value="Je m'inscris" />
+                 			</td>
+              			</tr>
+					</table>
+				</form>/
+
+
+			</div>
+		</body>
+
+</html>
