11ece9d05851eb1cecf8e85c87f0af6f1a3e9125
guigiubatt <guilhem.agussol@etu.umontpellier.fr>
2019-11-02 17:39:02 +0100

    ajout model jeu

---
+++ model/ModelJeu.php
@@ -0,0 +1,42 @@
+<? php
+	require_once 'Model.php';
+
+	class ModelJeu
+	{
+		private $id;
+		private $titre;
+		private $nbPost;
+
+	    //getter      
+	    public function getId() {
+	        return $this->id;  
+	    }
+	     public function getTitre() {
+	        return $this->titre;  
+	    }
+	     public function getNbPost() {
+	        return $this->nbPost;  
+	    }
+	       
+	    //setter 
+	    public function setId($id2) {
+	        $this->marque = $marque2;
+	    } 
+	    public function setTitre($titre2) {
+	        $this->titre =$titre2;
+	    }
+	    public function setNbPost($nbPost2) {
+	       	$this->nbPost = $nbPost2;
+	       
+	    }
+
+	    public function __construct($i = NULL, $t = NULL, $n = NULL) {
+		    if (!is_null($i) && !is_null($t) && !is_null($n)) {
+		        $this->id = $i;
+		        $this->titre = $t;
+		        $this->nbPost = $n;
+		    }
+		} 
+	}
+	
+?>
