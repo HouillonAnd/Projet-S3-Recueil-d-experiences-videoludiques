79d62a500752bb41681452f9d6d4fcb6b0ca6316
HouillonAnd <43677104+HouillonAnd@users.noreply.github.com>
2019-11-03 12:29:11 +0100

    Meilleure base du routeur -- still work to do
    Multiples controllers NON IMPLÉMENTÉS

--- controller/routeur.php
+++ controller/routeur.php
@@ -1,6 +1,20 @@
 <?php
-require_once File::build_path(array('controller','ControllerVoiture.php'));
-// On recupère l'action passée dans l'URL (à modifier methode POST)
-$action = $_GET['action'];	
-ControllerVoiture::$action(); 
+	require_once (File::build_path(array("controller","ControllerPost.php")));
+	
+	// On recupère l'action passée dans l'URL
+	
+	$class_methods = get_class_methods('ControllerPost');
+
+	if (isset($_GET['action'])){
+
+		if (in_array($_GET['action'],$class_methods)) {
+			// On recupère l'action passée dans l'URL
+			$action = $_GET['action'];
+			// Appel de la méthode statique $action de ControllerVoiture
+			ControllerPost::$action();
+
+		}else{
+			ControllerPost::error();
+		}
+	}
 ?>
