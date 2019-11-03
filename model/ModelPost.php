bf21232617f7a8e77101868dfb7335b724d0ecd5
HouillonAnd <43677104+HouillonAnd@users.noreply.github.com>
2019-11-03 12:14:39 +0100

    Fonctions principales de ModelPost

--- model/ModelPost.php
+++ model/ModelPost.php
@@ -75,7 +75,104 @@
 		        $this->emotion_id = $e;
 		        $this->nbUpvote = 0;
 		    }
-		} 
+		}
+
+    public static function getAllPost(){
+
+      $satement = "SELECT * FROM _S3_Post";
+      $rep = Model::$pdo -> query($satement);
+      
+      $rep->setFetchMode(PDO::FETCH_CLASS, 'ModelPost');
+      $tab_post = $rep->fetchAll();
+
+      return $tab_post;
+
+    }
+
+    public static function getPostById($id) {
+      $sql = "SELECT * from _S3_Post WHERE id=:nom_tag";
+      // Préparation de la requête
+      $req_prep = Model::$pdo->prepare($sql);
+
+      $values = array(
+          "nom_tag" => $id,
+          //nomdutag => valeur, ...
+      );
+      // On donne les valeurs et on exécute la requête   
+      $req_prep->execute($values);
+
+      // On récupère les résultats comme précédemment
+      $req_prep->setFetchMode(PDO::FETCH_CLASS, 'ModelPost');
+      $tab_post = $req_prep->fetchAll();
+      // Attention, si il n'y a pas de résultats, on renvoie false
+      if (empty($tab_post)){
+          return false;
+        }
+      return $tab_post[0];
+    }
+
+    public static function deleteById($id)
+    {
+      try {
+        $sql = "DELETE from _S3_Post WHERE id=:nom_tag";
+        // Préparation de la requête
+        $req_prep = Model::$pdo->prepare($sql);
+
+        $values = array(
+            "nom_tag" => $id,
+            //nomdutag => valeur, ...
+        );
+        // On donne les valeurs et on exécute la requête   
+        $req_prep->execute($values);
+        return true;
+      } catch (PDOException $e) {
+        return false;
+      }
+    }
+    
+
+    public function save() {
+    
+      $statement ="INSERT INTO _S3_Post(id, auteur_id,date_publication, contenu,jeu_id, titre, emotion_id, nbUpvote) VALUES(:id, :auteur_id, :date_publication, :contenu,:jeu_id, :titre, :emotion_id, :nbUpvote)";
+      $req_prep = Model::$pdo->prepare($statement);
+
+      $values = array(
+        "id" => $this->id,
+        "auteur_id" => $this->auteur_id,
+        "date_publication" => $this->date_publication,
+        "contenu" => $this->contenu,
+        "jeu_id" => $this->jeu_id,
+        "titre" => $this->titre,
+        "emotion_id" => $this->emotion_id,
+        "nbUpvote" => $this->nbUpvote
+      );    
+
+      $req_prep->execute($values); 
+    }
+
+    public function update($data) {
+      try {
+        $sql = "UPDATE _S3_Post SET auteur_id=:auteur_id , date_publication=:date_publication , contenu=:contenu , jeu_id=:jeu_id , titre=:titre , emotion_id=:emotion_id, nbUpvote=:nbUpvote WHERE id=:id";
+        $req_prep = Model::$pdo->prepare($sql);
+        $values = array(
+             "id" =>$this->id,
+             "auteur_id" => $data['auteur_id'],
+             "date_publication" => $data['date_publication'],
+             "contenu" => $data['contenu'],
+             "jeu_id" => $data['jeu_id'],
+             "titre" => $data['titre'],
+             "emotion_id" => $data['emotion_id'],
+             "nbUpvote" => $data['nbUpvote']
+            //nomdutag => valeur, ...
+        );
+        // On donne les valeurs et on exécute la requête   
+        $req_prep->execute($values);
+
+        return true;  
+      }catch (PDOException $e) {
+        return false;
+      }
+    } 
 	}
 	
 ?>
