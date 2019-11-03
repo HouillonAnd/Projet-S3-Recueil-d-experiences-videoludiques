6dfa19141b8630ac702681576f947a2cafb36eb3
HouillonAnd <43677104+HouillonAnd@users.noreply.github.com>
2019-11-03 11:34:28 +0100

    Fonctions principales de ModelEmotions
    getAllEmotions , getEmotionById, deleteById,  save, update

--- model/ModelEmotions.php
+++ model/ModelEmotions.php
@@ -67,7 +67,102 @@
 		        $this->surprise = $s;
 		        $this->degout = $d;
 		    }
-		} 
+		}
+
+    public static function getAllEmotions(){
+
+      $satement = "SELECT * FROM _S3_Emotions";
+      $rep = Model::$pdo -> query($satement);
+      
+      $rep->setFetchMode(PDO::FETCH_CLASS, 'ModelEmotions');
+      $tab_em = $rep->fetchAll();
+
+      return $tab_em;
+
+    }
+
+    public static function getEmotionById($id) {
+      $sql = "SELECT * from _S3_Emotions WHERE id=:nom_tag";
+      // Préparation de la requête
+      $req_prep = Model::$pdo->prepare($sql);
+
+      $values = array(
+          "nom_tag" => $id,
+          //nomdutag => valeur, ...
+      );
+      // On donne les valeurs et on exécute la requête   
+      $req_prep->execute($values);
+
+      // On récupère les résultats comme précédemment
+      $req_prep->setFetchMode(PDO::FETCH_CLASS, 'ModelEmotions');
+      $tab_em = $req_prep->fetchAll();
+      // Attention, si il n'y a pas de résultats, on renvoie false
+      if (empty($tab_em)){
+          return false;
+        }
+      return $tab_tv[0];
+    }
+
+    public static function deleteById($id)
+    {
+      try {
+        $sql = "DELETE from _S3_Emotions WHERE id=:nom_tag";
+        // Préparation de la requête
+        $req_prep = Model::$pdo->prepare($sql);
+
+        $values = array(
+            "nom_tag" => $id,
+            //nomdutag => valeur, ...
+        );
+        // On donne les valeurs et on exécute la requête   
+        $req_prep->execute($values);
+        return true;
+      } catch (PDOException $e) {
+        return false;
+      }
+    }
+    
+
+    public function save() {
+    
+      $statement ="INSERT INTO _S3_Emotions(id, tristesse,joie, colere,peur, surprise, degout) VALUES(:id, :tristesse, :joie, :colere,:peur, :surprise, :degout)";
+      $req_prep = Model::$pdo->prepare($statement);
+
+      $values = array(
+        "id" => $this->id,
+        "tristesse" => $this->tristesse,
+        "joie" => $this->joie,
+        "colere" => $this->colere,
+        "peur" => $this->peur,
+        "surprise" => $this->surprise,
+        "degout" => $this->degout,
+      );    
+
+      $req_prep->execute($values); 
+    }
+
+    public function update($data) {
+      try {
+        $sql = "UPDATE _S3_Emotions SET tristesse=:tristesse , joie=:joie , colere=:colere , peur=:peur , surprise=:surprise , degout=:degout WHERE id=:id";
+        $req_prep = Model::$pdo->prepare($sql);
+        $values = array(
+             "id" =>$this->id,
+             "tristesse" => $data['tristesse'],
+             "joie" => $data['joie'],
+             "colere" => $data['colere'],
+             "peur" => $data['peur'],
+             "surprise" => $data['surprise'],
+             "degout" => $data['degout']
+            //nomdutag => valeur, ...
+        );
+        // On donne les valeurs et on exécute la requête   
+        $req_prep->execute($values);
+
+        return true;  
+      }catch (PDOException $e) {
+        return false;
+      }
+    }
 	}
 	
 ?>
