784238e0ee2d2df79009b7980fc37a6db88eb116
kalil <kalil.pelissier@etu.umontpellier.fr>
2019-11-08 15:10:25 -0400

    Modificatoin du routeur pour qu'ils prennent en compte plusieurs controller

--- controller/routeur.php
+++ controller/routeur.php
@@ -1,20 +1,30 @@
 <?php
-	require_once (File::build_path(array("controller","ControllerPost.php")));
+	require_once File::build_path(array("controller","ControllerPost.php"));	
 	
 	// On recupère l'action passée dans l'URL
-	
-	$class_methods = get_class_methods('ControllerPost');
 
-	if (isset($_GET['action'])){
-
-		if (in_array($_GET['action'],$class_methods)) {
-			// On recupère l'action passée dans l'URL
-			$action = $_GET['action'];
-			// Appel de la méthode statique $action de ControllerVoiture
-			ControllerPost::$action();
-
-		}else{
-			ControllerPost::error();
-		}
+	if(!isset($_GET['action'])) {
+		$_GET['action'] = 'readAll';
 	}
+
+	if(!isset($_GET['controller'])) {
+		$_GET['controller'] = 'post';
+	}
+
+	// On recupère l'action et le controleur passée dans l'URL
+	$action = $_GET['action'];
+	$controller = $_GET['controller'];
+
+	//création du nom de classe
+	$controller_class = 'Controller'.ucfirst($controller);
+	
+	//récupères des méthodes du controller passer dans l'URL
+	$methodes = get_class_methods($controller_class);
+
+	//vérification si la class et l'action existe bien
+	if(class_exists($controller_class) && in_array($_GET['action'], $methodes)) {
+		$controller_class::$action();
+	}else {
+		$controller_class::error();
+	}	
 ?>
