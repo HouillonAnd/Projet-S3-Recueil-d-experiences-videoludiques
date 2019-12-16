8fa61c4083d721f3640775630337b9364f72a2f7
leonard <leonard.rivals@etu.umontpellier.fr>
2019-12-16 16:59:51 +0100

    fix les bails

--- view/view.php
+++ view/view.php
@@ -57,12 +57,11 @@
             }  ?>
       </ul>
     </div>
-    <form class="navbar-form navbar-left" action="/action_page.php">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Search">
-      </div>
-      <button type="submit" class="btn btn-default">Submit</button>
-    </form>
+
+    <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
   </nav>
 
 </div>
@@ -81,116 +80,21 @@
 
 <footer class="page-footer">
   <div class="card text-center">
-  <div class="card-header">
-    Footer
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Lien pour un discord? </a>
-  </div>
-  <div class="card-footer text-muted">
-    © <?php echo date("Y"); ?> REV
-  </div>
-</div>
-<!--
-  <div class="card bg-dark text-white">
-  <img src="image/footer.jpg" class="card-img" alt="AC">
-  <div class="card-img-overlay">
-    <h5 class="card-title"> Content</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <!--
-    <ul>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-        </ul>
-        -->
-    
-      <!--
-        <div class="container">
-      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-      
+    <div class="card-header">
+      Footer
     </div>
-    <p class="card-text">Last updated 3 mins ago</p>
-  </div>
-</div>
--->
-  <!--
-  <div class="container">
-    <div class="row">
-      <div class="col l6 s12">
-        <h5 class="white-text">Footer Content</h5>
-        <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-      </div>
-      <div class="col l4 offset-l2 s12">
-        <h5 class="white-text">Links</h5>
-        <ul>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-  <header>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="index.php">REV</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <?php
-          if (isset($_SESSION["login"])) {
-            echo ("<li class=\"nav-item\">
-                    <a class=\"nav-link\" href=\"index.php?controller=user&action=read\">Profil</a>
-                    </li>" .
-                                  "<li class=\"nav-item\">
-                    <a class=\"nav-link\" href=\"index.php?action=create\">Poster</a>
-                    </li>" .
-                                  "<li class=\"nav-item\">
-                    <a class=\"nav-link\" href=\"index.php?controller=user&action=deconnect\" tabindex=\"-1\">Logout</a>
-                    </li>");
-                              } else {
-                                echo ("<li class=\"nav-item\">
-                    <a class=\"nav-link\" href=\"index.php?controller=user&action=create\">Inscription</a>
-                    </li>" .
-                                  "<li class=\"nav-item\">
-                    <a class=\"nav-link\" href=\"index.php?controller=user&action=connect\" tabindex=\"-1\">Login</a>
-                    </li>");
-          }  ?>
-
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
-
-  </header>
-
-  <main>
-    <div class="container">
-      <?php
-      require File::build_path(array("view", $controller, "$view.php"));
-      ?>
+    <div class="card-body">
+      <h5 class="card-title">Special title treatment</h5>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="btn btn-primary">Lien pour un discord? </a>
     </div>
-  </main>
-
-  <footer class="page-footer">
-    <div class="container">
-
-    © <?php //echo date("Y"); ?> REV
-      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+    <div class="card-footer text-muted">
+      © <?php echo date("Y"); ?> REV
     </div>
   </div>
--->
 
+
+  
 </footer>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
@@ -199,8 +103,8 @@
     <!-- revoir pourquoi le builtpath ne fonctionne pas  -->
     <!-- <script src="<?php echo File::build_path(array('view','autocomplet.js'))?>"></script> -->
     <script src="view/autocomplet.js"></script>
-</body>
 
+    <!--
       <div class="row">
         <div class="col l6 s12">
           <h5 class="white-text">Footer Content</h5>
@@ -224,6 +128,7 @@
       </div>
     </div>
   </footer>
+  -->
 
 
   <!-- bootstrap -->
