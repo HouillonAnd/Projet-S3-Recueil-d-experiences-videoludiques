15e657f7c9650b7e43dc4f6b150cf202cbb6a051
Leonard-Rivals <47136441+Leonard-Rivals@users.noreply.github.com>
2019-12-28 16:43:33 +0100

    Update list.php
    Problème des auteurs

--- view/post/list.php
+++ view/post/list.php
@@ -4,7 +4,7 @@
 
     foreach ($tab_post as $p){
 	    $id=$p -> getId();
-		$auteur_login=$p -> getAuteur_id();
+		$auteur_id=$p -> getAuteur_id();
 		$date_publication=$p -> getDate_publication();
 		$contenu=$p -> getContenu();
 		$jeu_id=$p -> getJeu_id();
@@ -14,19 +14,23 @@
 
 		$jeu=ModelJeu::getJeuById($jeu_id);
 		$titre_jeu=$jeu->getTitre();
-
-		// echo("33".$auteur_login);
+		$emotion=ModelEmotions::getEmotionById($emotion_id);
+		// echo("33".$auteur_id);
 
 		$emotion=ModelEmotions::getEmotionById($emotion_id);
 		echo ("<div class=\"shadow-sm p-3 mb-5 bg-white rounded\">");
 		echo ("<div class=\"card\"> <div class=\"card-header\">".htmlspecialchars($titre)."</div>");
-		echo("<div class=\"card-body\"> <h5 class=\"card-title\">".htmlspecialchars($titre_jeu)."</h5> <p class=\"card-text\">".htmlspecialchars($contenu)."</p> <p>place pour les emotions</p> </div>");
+		echo("<div class=\"card-body\"> <h5 class=\"card-title\">".htmlspecialchars($titre_jeu)."</h5> 
+			<p class=\"card-text\">".htmlspecialchars($contenu)."</p> 
+			<p>tristesse ".htmlspecialchars($emotion->getTristesse())." joie ".htmlspecialchars($emotion->getJoie())." colère ".htmlspecialchars($emotion->getColere())." peur ".htmlspecialchars($emotion->getPeur())." surprise ".htmlspecialchars($emotion->getSurprise())." dégout ".htmlspecialchars($emotion->getDegout())." </p> </div>");
+		
+
 		echo("<div class=\"card-footer\">");
 
 		echo("<div class=\"row\"><div class=\"col-sm-6\"> <div class=\"card\">Upvote : ".htmlspecialchars($nbUpvote)." <button type=\"button\" class=\"btn btn-primary btn-sm\" data-toggle=\"button\" aria-pressed=\"false\">  Upvote </button></div></div>");
 
-		echo("<div class=\"col-sm-6\"> <div class=\"card\">Auteur : ".htmlspecialchars($auteur_login)."</div></div></div>");
-		if(isset($_SESSION["login"]) && $_SESSION["login"] == $auteur_login){
+		echo("<div class=\"col-sm-6\"> <div class=\"card\">Auteur : ".htmlspecialchars($auteur_id)."</div></div></div>");
+		if(isset($_SESSION["login"]) && $_SESSION["login"] == $auteur_id){
 			echo("<br><a href=index.php?action=update&id=" . rawurlencode($p->getId()) . '>' . 'MODIFIER' . '</a>');
 		}
 		echo("</div></div></div>");
