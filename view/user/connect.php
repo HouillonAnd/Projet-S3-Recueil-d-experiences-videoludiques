ebd0c0bf1356cac804ed73cb57917110b6cca373
kalil <kalil.pelissier@etu.umontpellier.fr>
2020-01-02 01:12:40 -0400

    update page de connextion user

--- view/user/connect.php
+++ view/user/connect.php
@@ -1,15 +1,21 @@
-<form action="" method="get" class="form-example">
-  <input type="hidden" name="controller" value="user">
-  <input type="hidden" name="action" value="connected">
-  <div class="form-example">
-    <label for="login">Login: </label>
-    <input type="text" name="login" id="login" value="<?php echo $login?>"required>
-  </div>
-  <div class="form-example">
-    <label for="password">Password: </label>
-    <input type="password" name="password" id="password" required>
-  </div>
-  <div class="form-example">
-    <input type="submit" value="Log!">
-  </div>
-</form>
+<div class="row justify-content-center">
+
+		<div class="card" style="width: 18rem;">
+		  <div class="card-body">
+		   	<form action="" method="get" class="form-example">
+         <input type="hidden" name="controller" value="user">
+        <input type="hidden" name="action" value="connected">
+			  <div class="form-group">
+			    <label for="exampleInputLogin">Login</label>
+			    <input type="text" name="login" class="form-control" id="exampleInputLogin" value="<?php echo htmlspecialchars($login)?>" required>
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputPassword1">Password</label>
+			    <input type="password" name="password" class="form-control" id="exampleInputPassword1" required>
+			  </div>
+			  <button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+		  </div>
+		</div>
+		
+	</div>
