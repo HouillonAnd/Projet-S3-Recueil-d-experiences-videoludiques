866737f1711ea69fc724656ae38a939a2ba007e4
Leonard-Rivals <47136441+Leonard-Rivals@users.noreply.github.com>
2020-01-02 14:07:26 +0100

    Update detail.php
    J'essaye d'afficher les post de l'utilisateur quand on vas sur le détail du profil mais c pas encore complet

--- view/user/detail.php
+++ view/user/detail.php
@@ -1,4 +1,10 @@
 <?php
+
+	require_once File::build_path(array('model', 'ModelJeu.php'));
+	require_once File::build_path(array('model', 'ModelEmotions.php'));
+	require_once File::build_path(array('model', 'ModelPost.php'));
+	
+
     $login = $v->getLogin();
     // $bar = <<<EOT
     // <div>
@@ -10,4 +16,54 @@
     
     echo $login;
     echo "<br><a href=index.php?controller=user&action=update&login=" . rawurlencode($v->getLogin()) . '>' . 'MODIFIER' . '</a>'.'<br>';
+
+    if (empty($tab_p)) {
+    	echo("Vous n'avez rien posté jusqu'a maintenant.");
+    }
+    else{
+    	echo("<h4> Vos Post : </h4>");
+    	var_dump($tab_p);
+    	foreach ($tab_p as $p){
+    		echo("a");
+    		var_dump($tab_p);
+		    $id=$p -> getId();
+			$auteur_id=$p -> getAuteur_id();
+			$date_publication=$p -> getDate_publication();
+			$contenu=$p -> getContenu();
+			$jeu_id=$p -> getJeu_id();
+			$titre=$p -> getTitre();
+			$emotion_id=$p -> getEmotion_id();
+			$nbUpvote=$p -> getnbUpvote();
+
+			$jeu=ModelJeu::getJeuById($jeu_id);
+			$titre_jeu=$jeu->getTitre();
+			$emotion=ModelEmotions::getEmotionById($emotion_id);
+			// echo("33".$auteur_id);
+
+			$emotion=ModelEmotions::getEmotionById($emotion_id);
+			echo ("<div class=\"shadow-sm p-3 mb-5 bg-white rounded\">");
+			echo ("<div class=\"card\"> <div class=\"card-header\">".htmlspecialchars($titre)."</div>");
+			echo("<div class=\"card-body\"> <h5 class=\"card-title\">".htmlspecialchars($titre_jeu)."</h5> 
+				<p class=\"card-text\">".htmlspecialchars($contenu)."</p> 
+
+			
+			<button type=\"button\" class=\"btn btn-outline-secondary\">tristesse ".htmlspecialchars($emotion->getTristesse())."</button>
+			<button type=\"button\" class=\"btn btn-outline-success\">joie ".htmlspecialchars($emotion->getJoie())."</button>
+			<button type=\"button\" class=\"btn btn-outline-danger\">colère ".htmlspecialchars($emotion->getColere())."</button>
+			<button type=\"button\" class=\"btn btn-outline-warning\">peur ".htmlspecialchars($emotion->getPeur())."</button>
+			<button type=\"button\" class=\"btn btn-outline-info\">surprise ".htmlspecialchars($emotion->getSurprise())."</button>
+			
+			<button type=\"button\" class=\"btn btn-outline-dark\">dégout ".htmlspecialchars($emotion->getDegout())."</button>");
+		
+
+			echo("<div class=\"card-footer\">");
+			echo("<div class=\"row\"><div class=\"col-sm-6\"> <div class=\"card\">Upvote : ".htmlspecialchars($nbUpvote)." <button type=\"button\" class=\"btn btn-primary btn-sm\" data-toggle=\"button\" aria-pressed=\"false\">  Upvote </button></div></div>");
+
+			echo("<div class=\"col-sm-6\"> <div class=\"card\">Auteur : ".htmlspecialchars($auteur_id)."</div></div></div>");
+			if(isset($_SESSION["login"]) && $_SESSION["login"] == $auteur_id){
+				echo("<br><a href=index.php?action=update&id=" . rawurlencode($p->getId()) . '>' . 'MODIFIER' . '</a>');
+			}
+			echo("</div></div></div></div>");
+    }
+    }
 ?>
