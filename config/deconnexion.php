918990fcc35d74b59540f9961d94f6fbfde51c17
kalil <kalil.pelissier@etu.umontpellier.fr>
2020-01-03 18:28:27 -0400

    retrait fichier inutile dans Conf

--- config/deconnexion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-$_SESSION = array();
-
-session_destroy();
-//renvoi à la page de connexion mais dans un second temps on pourrait simpler renvoyer vers la page d'acceuil.
-header("Location : connexion.php")
-
-?>
