d15d305b42597d2b7782637a2484f522acdd8c50
kalil <kalil.pelissier@etu.umontpellier.fr>
2020-01-04 20:29:00 -0400

    mise à jour formulaire des postes

--- view/post/form.php
+++ view/post/form.php
@@ -1,58 +1,93 @@
-<form method="get" action="./index.php">
-	<?php echo $_SESSION["login"]?>
-	<input type="hidden" name="action" value = "<?php echo $action?>">
-	<?php if($id != ""){echo "<input type=\"hidden\" name=\"id\" value = $id >";} 
-	if($date_publication != ""){echo "<input type=\"hidden\" name=\"date_publication\" value = $date_publication >";} ?>
-	<input type="hidden" name="auteur_id" value = <?php echo $auteur_id?>>
-	<input type="hidden" name="emotion_id" value = <?php echo $emotion_id?>>
-	<input type="hidden" name="nbUpvote" value = <?php echo $nbUpvote?>>
-	<input type="hidden" name="jeu_id" value = "<?php echo $jeu_id?>"> 
+<div class="row justify-content-center">
 
-	<label>Donnez un titre à votre post : </label>
-	<input type ="text" name="titre" placeholder="Titre" value= "<?php echo $titre?>" $option><br>
+	<div class="card" style="width: 30rem;">
+		<div class="card-body">
+			<form method="get" action="./index.php">
+				<input type="hidden" name="action" value="<?php echo $action ?>">
+				<?php if ($id != "") {
+					echo "<input type=\"hidden\" name=\"id\" value = $id >";
+				}
+				if ($date_publication != "") {
+					echo "<input type=\"hidden\" name=\"date_publication\" value = $date_publication >";
+				} ?>
+				<input type="hidden" name="auteur_id" value=<?php echo $auteur_id ?>>
+				<input type="hidden" name="emotion_id" value=<?php echo $emotion_id ?>>
+				<input type="hidden" name="nbUpvote" value=<?php echo $nbUpvote ?>>
+				<input type="hidden" name="jeu_id" value="<?php echo $jeu_id ?>">
 
-	<label>Titre du jeu</label>
-	<input type ="text" name="jeu_titre"  id="gametitle" placeholder="Titre du jeu" autocomplete="off" <?php echo "$option=true"." value=".($jeu == false? " ":$jeu->getTitre())?>><br>
+				<div class="form-group">
+					<label for="InputTitle">Titre</label>
+					<input type="text" name="titre" class="form-control" id="InputTitle" value="<?php echo $titre ?>" aria-describedby="emailHelp" $option>
+					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+				</div>
 
-	<div id="search-result">
+				<div class="form-group">
+					<label for="gametitle">Jeu</label>
+					<input type="text" name="jeu_titre" class="form-control" id="gametitle" autocomplete="off" value="<?php echo ($jeu == false ? " " : $jeu->getTitre()) ?>" aria-describedby="emailHelp" $option>
+					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+					<div id="search-result">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label for="Textarea">Description</label>
+					<textarea class="form-control" name="contenu" id="Textarea" rows="3" required><?php echo $contenu ?></textarea>
+					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+				</div>
+
+				<div class="form-group">
+					<label>Tristesse</label>
+					<input type="range" list="tickmarks" class="PostRange" name="tristesse" min=0 max=100 value="<?php echo ($emotion == false ? "0" : $emotion->getTristesse()) ?>" required>
+					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+				</div>
+
+				<div class="form-group">
+					<label>Joie</label>
+					<input type="range" list="tickmarks" class="PostRange" name="joie" min=0 max=100 value="<?php echo ($emotion == false ? "0" : $emotion->getJoie()) ?>" required>
+					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+				</div>
+
+				<div class="form-group">
+					<label>Colère</label>
+					<input type="range" list="tickmarks" class="PostRange" name="colere" min=0 max=100 value="<?php echo ($emotion == false ? "0" : $emotion->getColere()) ?>" required>
+					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+				</div>
+
+				<div class="form-group">
+					<label>Peur</label>
+					<input type="range" list="tickmarks" class="PostRange" name="peur" min=0 max=100 value="<?php echo ($emotion == false ? "0" : $emotion->getPeur()) ?>" required>
+					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+				</div>
+
+				<div class="form-group">
+					<label>Surprise</label>
+					<input type="range" list="tickmarks" class="PostRange" name="surprise" min=0 max=100 value="<?php echo ($emotion == false ? "0" : $emotion->getSurprise()) ?>" required>
+					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+				</div>
+
+				<div class="form-group">
+					<label>Dégoût</label>
+					<input type="range" list="tickmarks" class="PostRange" name="degout" min=0 max=100 value="<?php echo ($emotion == false ? "0" : $emotion->getDegout()) ?>" required>
+					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+				</div>
+
+				<datalist id="tickmarks">
+					<option value="0">
+					<option value="10">
+					<option value="20">
+					<option value="30">
+					<option value="40">
+					<option value="50">
+					<option value="60">
+					<option value="70">
+					<option value="80">
+					<option value="90">
+					<option value="100">
+				</datalist>
+
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+		</div>
 	</div>
-	
-	<label>Que pensez vous de ce Jeu ?: </label>
-	<textarea name="contenu" placeholder="Description" required><?php echo $contenu?></textarea><br>
 
-	<!-- Expression des emotions sous forme d'entrée de valeurs.-->
-	<label>Sur une échelle de 0 à 100 mesurez: </label><br>
-	<label>-la tristesse procurée par le jeu :  </label>
-	<input type="range" list="tickmarks"  class="PostRange"name="tristesse" min=0 max=100 value= "<?php echo($emotion == false?"0":$emotion->getTristesse())?>" required><br>
-	<label>-la joie procurée par le jeu :  </label>
-	<input type="range" list="tickmarks"  class="PostRange"name="joie" min=0 max=100 value= "<?php echo($emotion == false?"0":$emotion->getJoie())?>" required><br>
-	<label>-la colère procurée par le jeu :  </label>
-	<input type="range" list="tickmarks"  class="PostRange"name="colere" min=0 max=100 value= "<?php echo($emotion == false?"0":$emotion->getColere())?>" required><br>
-	<label>-la peur procurée par le jeu :  </label>
-	<input type="range" list="tickmarks"  class="PostRange"name="peur" min=0 max=100 value= "<?php echo($emotion == false?"0":$emotion->getPeur())?>" required><br>
-	<label>-la surprise procurée par le jeu :  </label>
-	<input type="range" list="tickmarks"  class="PostRange"name="surprise" min=0 max=100 value= "<?php echo($emotion == false?"0":$emotion->getSurprise())?>" required><br>
-	<label>-le dégoût procuré par le jeu :  </label>
-	<input type="range" list="tickmarks"  class="PostRange"name="degout" min=0 max=100  value= "<?php echo($emotion == false?"0":$emotion->getDegout())?>" required><br>
-
-	<!-- <input type="range" list="tickmarks" class="PostRange"> -->
-
-	<datalist id="tickmarks">
-	  <option value="0">
-	  <option value="10">
-	  <option value="20">
-	  <option value="30">
-	  <option value="40">
-	  <option value="50">
-	  <option value="60">
-	  <option value="70">
-	  <option value="80">
-	  <option value="90">
-	  <option value="100">
-	</datalist>
-
-	
-	<input type ="submit" value="Post"><br>
-</form>
-
-
+</div>
