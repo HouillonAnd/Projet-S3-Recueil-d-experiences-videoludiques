562734f10816be50d7a06d15c6306dc6c50c84e6
guigiubatt <guilhem.agussol@etu.umontpellier.fr>
2020-01-05 20:42:04 +0100

    couleur header
    modif couleur bg header et boutton search

--- view/view.php
+++ view/view.php
@@ -18,8 +18,8 @@
 <body>
 
 <header>
-  <div class="shadow p-3 mb-5 bg-white rounded">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="shadow p-3 mb-5 rounded">
+    <nav class="navbar navbar-expand-lg navbar-light ">
     <a class="navbar-brand" href="index.php?controller=post&action=readAll">REV</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -67,7 +67,7 @@
 
     <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
         </form>
   </nav>
 
